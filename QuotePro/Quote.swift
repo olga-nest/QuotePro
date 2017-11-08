dc74d987df5c6bb7e9c2ee97add836971f7d0f31
olga-nest <olga.w.nesterova@gmail.com>
2017-11-08 12:26:58 -0800

    Create Quote class

---
+++ QuotePro/Quote.swift
@@ -0,0 +1,18 @@
+//
+//  Quote.swift
+//  QuotePro
+//
+//  Created by Olga on 11/8/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Quote {
+    
+    //MARK: Properties
+    var quoteText: String? = nil
+    var quoteAuthor: String? = nil
+    var quotePhoto: UIImage? = nil
+}
