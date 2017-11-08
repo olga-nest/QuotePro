1823584de51c7324fb3bdcbf8ef97e093e20e78f
olga-nest <olga.w.nesterova@gmail.com>
2017-11-08 12:21:25 -0800

    Create QuoteBuilderViewController class, connect outlets

---
+++ QuotePro/QuoteBilderViewController.swift
@@ -0,0 +1,39 @@
+//
+//  QuoteBilderViewController.swift
+//  QuotePro
+//
+//  Created by Olga on 11/8/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+import UIKit
+
+class QuoteBilderViewController: UIViewController {
+    
+    
+    @IBOutlet weak var quoteView: QuoteView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func changePhoto(_ sender: UIButton) {
+    }
+    
+    @IBAction func changeQuote(_ sender: UIButton) {
+    }
+    
+    @IBAction func saveQuote(_ sender: UIButton) {
+    }
+}
