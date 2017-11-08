6d19025cc6e4af1cccfd3dc3109b7183e8826687
olga-nest <olga.w.nesterova@gmail.com>
2017-11-08 11:50:13 -0800

    Add QuoteView class for xib

---
+++ QuotePro/QuoteView.swift
@@ -0,0 +1,14 @@
+//
+//  QuoteView.swift
+//  QuotePro
+//
+//  Created by Olga on 11/8/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+import UIKit
+
+class QuoteView: UIView {
+
+
+}
