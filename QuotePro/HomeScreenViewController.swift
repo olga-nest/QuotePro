7389357935cbacb0475ce54aa9a9674f128bec51
olga-nest <olga.w.nesterova@gmail.com>
2017-11-08 20:53:50 -0800

    Add HomeScreenViewController

---
+++ QuotePro/HomeScreenViewController.swift
@@ -0,0 +1,58 @@
+//
+//  HomeScreenViewController.swift
+//  QuotePro
+//
+//  Created by Olga on 11/8/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreenViewController: UITableViewController {
+    
+    //MARK: Properties
+    var savedQuotes = [Quote]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    //MARK: Table view
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return savedQuotes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "quoteCell", for: indexPath) as? QuoteTableViewCell
+            else {
+                fatalError("The dequeued cell is not an instance of QuoteTableViewCell")
+        }
+        
+        let quote = savedQuotes[indexPath.row]
+        cell.quote = quote
+    
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
